f5402c1accfa371f69ed1ca64708d53e034d6fc9
janelle-and <janelle.andrews@rdpolytech.ca>
2026-04-13 15:43:32 -0600

    employee page working

--- employee_training_session_analytics/analytics_portal/urls.py
+++ employee_training_session_analytics/analytics_portal/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.test),
-    path('employees/',views.employee_list,name="employee_list")
+    path('employees/',views.employee_list,name="employee_list"),
+    path('employees/add/', views.add_employee, name="add_employee"),
+    path('employees/update/<int:id>/', views.update_employee, name="update_employee"),
+    path('employees/delete/<int:id>/', views.delete_employee, name="delete_employee"),
 ]
--- employee_training_session_analytics/analytics_portal/views.py
+++ employee_training_session_analytics/analytics_portal/views.py
@@ -1,12 +1,42 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Employee, Enrollment
+from .forms import EmployeeForm
 
 # Create your views here.
 def test(request):
-    return render(request, 'analytics_portal/test.html')
+    return render(request, "analytics_portal/test.html")
 
 def employee_list(request):
     employees = Employee.objects.all()
 
-    return render(request, 'analytics_portal/employee/employee_list.html', {"employees":employees})
+    return render(request, "analytics_portal/employee/employee_list.html", {"employees":employees})
+
+def add_employee(request):
+    form = EmployeeForm()
+
+    if request.method == "POST":
+        form = EmployeeForm(request.POST,)
+        if form.is_valid():
+            form.save()
+            return redirect("employee_list")
+        
+    return render(request, "analytics_portal/employee/add_employee.html", {"form":form})
+
+def update_employee(request, id):
+    employee = Employee.objects.get(id = id)
+    form = EmployeeForm(instance=employee)
+
+    if request.method == "POST":
+        form = EmployeeForm(request.POST,instance=employee)
+        if form.is_valid():
+            form.save()
+            return redirect("employee_list")
+    
+    return render(request, "analytics_portal/employee/update_employee.html",{"form":form})
+
+def delete_employee(request, id):
+    employee = Employee.objects.get(id = id)
+    employee.delete()
+
+    return redirect("employee_list")
