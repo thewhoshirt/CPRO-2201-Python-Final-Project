3f82b7af8a9523e65e3a0ef3925cc77f348cbb7c
janelle-and <janelle.andrews@rdpolytech.ca>
2026-04-13 14:10:40 -0600

    started models

--- employee_training_session_analytics/analytics_portal/models.py
+++ employee_training_session_analytics/analytics_portal/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+class Employee(models.Model):
+    department_choices = [("1", "IT"),("2", "HR"),("3", "Sales"),(4,"Admin"),(5,"Management")]
+
+    full_name = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    department = models.CharField(choices=department_choices)
+
+    def __str__(self):
+        return self.full_name
+    
+class Enrollment(models):
+    status_choices = [(1,"ENROLLED"),(2,"COMPLETED"),(3,"CANCELLED")]
+
+    employee = models.ForeignKey(Employee, on_delete=models.CASCADE, related_name="enrollments")
+    session = models.ForeignKey(Session, on_delete=models.CASCADE, related_name="enrollments")
+    status = models.CharField(choices=status_choices)
+
+    def __str__(self):
+        return self.employee
